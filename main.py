d341db169b327ada38a6ce6b1fb121d8ccf2d2fd
kelasrayhan <rayhanrwa1@gmail.com>
2023-10-26 20:47:45 +0700

    Latihan input nilai

---
+++ main.py
@@ -0,0 +1,7 @@
+mat = int(input("Masukan Nilai Matematika : "))
+fis = int(input("Masukan Nilai Fisika : "))
+geo = int(input("Masukan Nilai Geografi: "))
+
+print("\nMatematika : ", mat)
+print("Fisika : ", fis)
+print("Geografi : ", geo)
