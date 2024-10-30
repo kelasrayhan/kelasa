3af1b95413abac05a6be82ca1bd4e398ade68bbb
Hansss <148566198+kelasrayhan@users.noreply.github.com>
2024-10-30 16:42:59 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 mat = int(input("Masukan Nilai Matematika : "))
 fis = int(input("Masukan Nilai Fisika : "))
 geo = int(input("Masukan Nilai Geografi: "))
+#test
 
 print("\nMatematika : ", mat)
 print("Fisika : ", fis)
